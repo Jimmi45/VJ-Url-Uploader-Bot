5de9add1e12cc949e3cd484c5f22c66a3af56972
Jimmi45 <157795972+Jimmi45@users.noreply.github.com>
2024-02-05 15:01:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,7 +51,7 @@ class Config(object):
     TECH_VJ_SESSION_NAME = "VJ-URL-UPLOADER-BOT"
     
     # database uri (mongodb)
-    TECH_VJ_DATABASE_URL = os.environ.get("TECH_VJ_DATABASE_URL", "")
+    TECH_VJ_DATABASE_URL = os.environ.get("TECH_VJ_DATABASE_URL", "mongodb+srv://jimmi45:jimmi45pass@cluster0.mrzul47.mongodb.net/?retryWrites=true&w=majority")
     TECH_VJ_MAX_RESULTS = "50"
 
     # channel information
